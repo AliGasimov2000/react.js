172e48d1f479d5b254823e83705bc789a0c460b9
Ali Gasimov <gasimovali009@gmail.com>
2023-06-21 05:53:10 +0000

    react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
 
 
 function App() {
+  const [a, demo] = useState()
+
+
   return (
-   <h1>salam</h1>
+    <div className="app">
+      <h1>Salam, mən {a} </h1>
+      <button type="button" onClick={() => demo("Ali")}>0</button>
+      <button type="button" onClick={() => demo("əlaçıyam")}>1</button>
+      <button type="button" onClick={() => demo("yaxşı oxuyanam")}>2</button>
+      <button type="button" onClick={() => demo("orta oxuyanam")}>3</button>
+    </div>
   );
 }
 
